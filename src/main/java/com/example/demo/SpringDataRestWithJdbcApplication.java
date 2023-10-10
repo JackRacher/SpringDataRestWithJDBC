ffafdd260e2fa22c6fca6599bfd9cec7ee079519
pb21071 <pb21071@ojas-dl529.corp.ojas-it.com>
2023-10-10 11:52:13 +0530

    Added files

---
+++ src/main/java/com/example/demo/SpringDataRestWithJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestWithJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestWithJdbcApplication.class, args);
+	}
+
+}
