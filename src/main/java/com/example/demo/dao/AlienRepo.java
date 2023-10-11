b19d63d18fb45bcd4b745581ab2ad3e2b368f0ae
pb21071 <pb21071@ojas-dl529.corp.ojas-it.com>
2023-10-11 09:51:05 +0530

    updated the code

--- src/main/java/com/example/demo/dao/AlienRepo.java
+++ src/main/java/com/example/demo/dao/AlienRepo.java
@@ -1,12 +1,19 @@
 package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.demo.models.Alien;
+import java.util.List;
+
 
 @RepositoryRestResource(collectionResourceRel = "aliens", path="aliens")
-public interface AlienRepo extends JpaRepository<Alien, Integer>{
+public interface AlienRepo extends JpaRepository<Alien, Integer>
+{
+	//@RestResource(path = "name", rel = "name")
+	List<Alien> findByAname(@Param("name") String aname);
 	
 
 }
