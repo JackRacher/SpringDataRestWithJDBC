2027841c37bd9d9f060fcb42a70cb61b26c9cbe7
pb21071 <pb21071@ojas-dl529.corp.ojas-it.com>
2024-05-15 10:16:27 +0530

    new Files added

--- src/main/java/com/example/demo/dao/AlienRepo.java
+++ src/main/java/com/example/demo/dao/AlienRepo.java
@@ -3,8 +3,6 @@ package com.example.demo.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
-
 import com.example.demo.models.Alien;
 import java.util.List;
 
@@ -14,6 +12,4 @@ public interface AlienRepo extends JpaRepository<Alien, Integer>
 {
 	//@RestResource(path = "name", rel = "name")
 	List<Alien> findByAname(@Param("name") String aname);
-	
-
 }
